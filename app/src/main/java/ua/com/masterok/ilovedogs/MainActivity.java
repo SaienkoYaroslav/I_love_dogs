5c225d0f5e89a0ddb387efce98e6047439794eb5
SaienkoYaroslav <saienko.yaroslav01@gmail.com>
2022-07-30 21:12:08 +0300

    Glide and Rx

--- app/src/main/java/ua/com/masterok/ilovedogs/MainActivity.java
+++ app/src/main/java/ua/com/masterok/ilovedogs/MainActivity.java
@@ -1,16 +1,25 @@
 package ua.com.masterok.ilovedogs;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.bumptech.glide.Glide;
+
 public class MainActivity extends AppCompatActivity {
 
+    private Button buttonNextImage;
+    private ImageView imageViewDogImage;
+    private ProgressBar progressBar;
+
     private MainViewModel mainViewModel;
-    private static final String TAG = "MainActivity";
 
 
     @Override
@@ -18,18 +27,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-        mainViewModel.loadDogImage();
-        mainViewModel.getDogImage().observe(this, new Observer<DogImage>() {
-            @Override
-            public void onChanged(DogImage dogImage) {
-                Log.d(TAG, dogImage.toString());
-            }
-        });
+        viewModel();
+        onClickButtonNextImage();
 
     }
 
     private void init() {
+        buttonNextImage = findViewById(R.id.button_load_next_image);
+        imageViewDogImage = findViewById(R.id.image_view_dog_image);
+        progressBar = findViewById(R.id.progress_bar);
+
+
+    }
+
+    private void viewModel() {
         mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        mainViewModel.loadDogImage();
+        mainViewModel.getIsLoading().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean loading) {
+                if (loading) {
+                    progressBar.setVisibility(View.VISIBLE);
+                } else {
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+        mainViewModel.getIsEthernetWorking().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean isEthernetWorking) {
+                if (!isEthernetWorking) {
+                    Toast.makeText(MainActivity.this, R.string.ethernet_error, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        mainViewModel.getDogImage().observe(this, new Observer<DogImage>() {
+            @Override
+            public void onChanged(DogImage dogImage) {
+                // Завантаження картинки з інтернету і вставка в ІВ за допомогою бібліотеки Глайд
+                Glide.with(MainActivity.this)
+                        .load(dogImage.getMessage())
+                        .into(imageViewDogImage);
+            }
+        });
+    }
+
+    private void onClickButtonNextImage() {
+        buttonNextImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainViewModel.loadDogImage();
+            }
+        });
     }
 
 
--- app/src/main/java/ua/com/masterok/ilovedogs/MainViewModel.java
+++ app/src/main/java/ua/com/masterok/ilovedogs/MainViewModel.java
@@ -21,6 +21,7 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Action;
 import io.reactivex.rxjava3.functions.Consumer;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
@@ -33,6 +34,8 @@ public class MainViewModel extends AndroidViewModel {
 
     private MutableLiveData<DogImage> dogImage = new MutableLiveData<>();
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
+    private MutableLiveData<Boolean> isLoading = new MutableLiveData<>();
+    private MutableLiveData<Boolean> isEthernetWorking = new MutableLiveData<>();
 
     public MainViewModel(@NonNull Application application) {
         super(application);
@@ -42,10 +45,41 @@ public class MainViewModel extends AndroidViewModel {
         return dogImage;
     }
 
+    public LiveData<Boolean> getIsLoading() {
+        return isLoading;
+    }
+
+    public LiveData<Boolean> getIsEthernetWorking() {
+        return isEthernetWorking;
+    }
+
     public void loadDogImage() {
         Disposable disposable = loadDogImageRx()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
+                // Все що написано в accept() виконається в момент підписки
+                .doOnSubscribe(new Consumer<Disposable>() { // .doOnSubscribe - колбек. В колбеці
+                    // ми реагуємо на певні стани
+                    @Override
+                    public void accept(Disposable disposable) throws Throwable {
+                        isLoading.setValue(true);
+                        isEthernetWorking.setValue(true);
+                    }
+                })
+                // Виконається після завантаження (успішного, чи не успішного). Для виконання коду тільки
+                // при успішному, чи тільки при не успішному є інші методи
+                .doAfterTerminate(new Action() { // .doOnSubscribe - колбек
+                    @Override
+                    public void run() throws Throwable {
+                        isLoading.setValue(false);
+                    }
+                })
+                .doOnError(new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Throwable {
+                        isEthernetWorking.setValue(false);
+                    }
+                })
                 .subscribe(new Consumer<DogImage>() {
                     @Override
                     public void accept(DogImage dogImageFromRx) throws Throwable {
